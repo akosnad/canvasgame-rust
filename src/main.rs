a1845cf2e6158732786b1dc53ee6e29d0871c23c
akosnad <akos@nadudvari.org>
2021-05-30 18:31:16 +0200

    main: remove unnecessary double-check

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,9 @@
 
 
 fn main() {
-    use minifb::Key;
     use canvasgame_rust::engine::Engine;
 
     let world = canvasgame_rust::world::World::new();
     let mut engine = canvasgame_rust::engine::native::NativeEngine::new(world);
-    while engine.window.is_open() && !engine.window.is_key_down(Key::Escape) {
-        engine.engine_loop();
-    }
-
+    engine.engine_loop();
 }
